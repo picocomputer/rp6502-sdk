634c60c3b981a1a083a05b38e11868ae7ab5154e
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2023-05-05 00:19:08 -0700

    lseek translates cc65 whence to posix

--- src/lseek.c
+++ src/lseek.c
@@ -7,6 +7,7 @@
 
 #include "rp6502.h"
 #include <unistd.h>
+#include <stdio.h>
 #include "errno.h"
 
 long __fastcall__ lseek32(long offset, char whence, int fildes)
@@ -41,5 +42,18 @@ long __fastcall__ lseek16(int offset, char whence, int fildes)
 
 off_t __fastcall__ lseek(int fd, off_t offset, int whence)
 {
+    // Change CC65 whence to what everyone else uses
+    switch (whence)
+    {
+    case SEEK_SET:
+        whence = 0;
+        break;
+    case SEEK_END:
+        whence = 2;
+        break;
+    case SEEK_CUR:
+        whence = 1;
+        break;
+    }
     return lseek32(offset, whence, fd);
 }
