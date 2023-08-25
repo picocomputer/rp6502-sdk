feb292270550968ab6beae79d66ac6515b315aa5
Brent Bettis <brent.bettis@gmail.com>
2023-08-25 13:30:03 -0700

    changing set_rtc_time to not send size as param, cleanup

--- src/rtc.c
+++ src/rtc.c
@@ -7,7 +7,7 @@ int __fastcall__ get_rtc_time(datetime_t *datetime)
     uint8_t *ptr;
     unsigned i;
     int ax;
-    uint8_t count = sizeof(datetime_t);
+    uint8_t count = sizeof(*datetime);
     RIA_OP = RIA_OP_GET_RTC;
     RIA_BLOCK();
     ptr = (uint8_t *)datetime;
@@ -16,7 +16,7 @@ int __fastcall__ get_rtc_time(datetime_t *datetime)
         return _mappederrno(RIA_ERRNO_LO);
     for (i = 0; i < count; i++)
         *ptr++ = RIA_VSTACK;
-    return ax;
+    return 0;
 
 }
 
@@ -25,14 +25,14 @@ int __fastcall__ set_rtc_time(const datetime_t *datetime)
     const uint8_t *ptr;
     uint8_t i;
     int ax;
-    uint8_t count = sizeof(datetime_t);
+    uint8_t count = sizeof(*datetime);
     ptr = ((uint8_t *)datetime) + count - 1;
     for (i = 0;i < count; ++i)
-        RIA_VSTACK = (uint8_t *)*ptr--;
-    RIA_CALL_A(RIA_OP_SET_RTC, count);
+        RIA_VSTACK = (uint8_t)(uint8_t *)*ptr--;
+    RIA_CALL(RIA_OP_SET_RTC);
     RIA_BLOCK();
     ax = RIA_AX;
     if (ax < 0)
         return _mappederrno(RIA_ERRNO_LO);
-    return ax;
+    return 0;
 }
